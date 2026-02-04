154112d44c5ff862591d0b7472a9d9f84b2bd17e
Peter Feerick <5500713+pfeerick@users.noreply.github.com>
2026-02-04 18:33:01 +1000

    fix: set flash port to 8000 (again!)
    accidently reverted in last commit

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def load_config():
             },
             'flask': {
                 'host': '0.0.0.0',
-                'port': 5000,
+                'port': 8000,
                 'debug': True
             },
             'dashboard': {
